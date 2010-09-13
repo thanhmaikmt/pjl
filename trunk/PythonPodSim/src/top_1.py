8af6b1c7fe9114f5937e875fa113622924e61e47
PaulJohnLeonard <PaulJohnLeonard@fadc175c-ebca-11de-a75d-bbceaaa5444e>
2010-09-13 09:18:26 +0000

    initial import

---
+++ trunk/PythonPodSim/src/top_1.py
@@ -0,0 +1,39 @@
+
+
+from simulation import *
+
+
+yHover   =  300
+dydtMin  = -100
+dydtMax  =  10
+
+class AutoControl:
+
+    def process(self,sensor,state):
+
+        control=Control()
+
+        if state.y > yHover:
+           if state.dydt > dydtMin:
+              control.up=1
+
+        if state.dydt > dydtMax:
+            control.up=1
+
+        return control
+
+
+
+dt          =.1
+brain       = AutoControl()
+nSensors    = 40
+sensorRange = 2000
+pod         = GravityPod(nSensors,sensorRange,brain,(255,0,0))
+pods        = [pod]
+world       = World("rect_world.txt",pods)
+sim         = Simulation(world,dt)
+
+#uncomment the next line to hide the walls.
+#sim.world.blind=True
+
+sim.run()
